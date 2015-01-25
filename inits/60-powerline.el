45b8f52970e9471c4104c10975a67ed999ad502c
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-01-25 21:49:53 +0900

    mode-line更新

--- inits/60-powerline.el
+++ inits/60-powerline.el
@@ -116,14 +116,17 @@ static char * arrow_right[] = {
 
                     ;; Justify right by filling with spaces to right fringe - 16
                     ;; (16 should be computed rahter than hardcoded)
-                    '(:eval (propertize " " 'display '((space :align-to (- right-fringe 34)))))
+                    '(:eval (propertize " " 'display '((space :align-to (- right-fringe 40)))))
 
                     '(:eval (concat (propertize " " 'display arrow-left-2)
                                     (propertize "[" 'face 'mode-line-color-2)
                                     (propertize (nyan-create) 'face 'mode-line-color-2)
                                     (propertize "]" 'face 'mode-line-color-2)))
                     '(:eval (concat (propertize " " 'display arrow-left-1)
-                                    (propertize "%4l:%2c  " 'face 'mode-line-color-1)))))
+                                    (propertize "%4l:%2c " 'face 'mode-line-color-1)
+                                    (propertize "(" 'face 'mode-line-color-1)
+                                    (propertize (number-to-string (point)) 'face 'mode-line-color-1)
+                                    (propertize ") " 'face 'mode-line-color-1)))))
 
 (make-face 'mode-line-color-1)
 (set-face-attribute 'mode-line-color-1 nil
