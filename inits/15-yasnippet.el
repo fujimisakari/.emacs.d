ab0d4cc0bd82ad4fe1631f76eda7c3c3361e0a21
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-03-19 23:22:22 +0900

    yasnippetを無効

--- inits/15-yasnippet.el
+++ inits/15-yasnippet.el
@@ -4,14 +4,14 @@
 ;;                               yasnippet設定                                ;;
 ;;;--------------------------------------------------------------------------;;;
 
-(require 'yasnippet)
-;; ~/.emacs.d/にsnippetsというフォルダを作っておきましょう
-(setq yas-snippet-dirs
-      '("~/.emacs.d/share/snippets"))
-(yas-global-mode 1)
-(yas-load-directory "~/.emacs.d/share/snippets")
+;; (require 'yasnippet)
+;; ;; ~/.emacs.d/にsnippetsというフォルダを作っておきましょう
+;; (setq yas-snippet-dirs
+;;       '("~/.emacs.d/share/snippets"))
+;; (yas-global-mode 1)
+;; (yas-load-directory "~/.emacs.d/share/snippets")
 
-;; 単語展開キーバインド (ver8.0から明記しないと機能しない)
-;; (setqだとtermなどで干渉問題ありでした)
-;; もちろんTAB以外でもOK 例えば "C-;"とか
-(custom-set-variables '(yas-trigger-key "TAB"))
+;; ;; 単語展開キーバインド (ver8.0から明記しないと機能しない)
+;; ;; (setqだとtermなどで干渉問題ありでした)
+;; ;; もちろんTAB以外でもOK 例えば "C-;"とか
+;; (custom-set-variables '(yas-trigger-key "TAB"))
