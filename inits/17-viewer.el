4e91c5fde15119da4496e00e69e312560587bb3d
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-04-11 00:43:35 +0900

    viewer設定更新

--- inits/17-viewer.el
+++ inits/17-viewer.el
@@ -61,23 +61,23 @@
   (interactive "P")
   (goto-line (line-number-at-pos (point-max))))
 
-;; 書き込み不能なファイルはview-modeで開くように
-(defadvice find-file
-  (around find-file-switch-to-view-file (file &optional wild) activate)
-  (if (and (not (file-writable-p file))
-           (not (file-directory-p file)))
-      (view-file file)
-    ad-do-it))
+;; ;; 書き込み不能なファイルはview-modeで開くように
+;; (defadvice find-file
+;;   (around find-file-switch-to-view-file (file &optional wild) activate)
+;;   (if (and (not (file-writable-p file))
+;;            (not (file-directory-p file)))
+;;       (view-file file)
+;;     ad-do-it))
 
-;; 書き込み不能な場合はview-modeを抜けないように
-(defvar view-mode-force-exit nil)
-(defmacro do-not-exit-view-mode-unless-writable-advice (f)
-  `(defadvice ,f (around do-not-exit-view-mode-unless-writable activate)
-     (if (and (buffer-file-name)
-              (not view-mode-force-exit)
-              (not (file-writable-p (buffer-file-name))))
-         (message "File is unwritable, so stay in view-mode.")
-       ad-do-it)))
+;; ;; 書き込み不能な場合はview-modeを抜けないように
+;; (defvar view-mode-force-exit nil)
+;; (defmacro do-not-exit-view-mode-unless-writable-advice (f)
+;;   `(defadvice ,f (around do-not-exit-view-mode-unless-writable activate)
+;;      (if (and (buffer-file-name)
+;;               (not view-mode-force-exit)
+;;               (not (file-writable-p (buffer-file-name))))
+;;          (message "File is unwritable, so stay in view-mode.")
+;;        ad-do-it)))
 
-(do-not-exit-view-mode-unless-writable-advice view-mode-exit)
-(do-not-exit-view-mode-unless-writable-advice view-mode-disable)
+;; (do-not-exit-view-mode-unless-writable-advice view-mode-exit)
+;; (do-not-exit-view-mode-unless-writable-advice view-mode-disable)
