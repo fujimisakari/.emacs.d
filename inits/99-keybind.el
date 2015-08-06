96c81a8caed800569b93f1f638629c59401c6ad3
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-08-07 03:36:15 +0900

    キーバインド追加

--- inits/99-keybind.el
+++ inits/99-keybind.el
@@ -45,6 +45,8 @@
 (bind-key "C-M-;" 'recenter-top-bottom)                      ; 現在の行の位置調整
 (bind-key "C-M-'" 'delete-other-windows)                     ; 現在のウィンドウ以外を消す
 (bind-key "C-M-," 'helm-gtags-find-tag-other-window)         ; (別バッファで)関数の定義元(関数の実体)へジャンプ
+(bind-key "C-M-." 'helm-gtags-find-rtag)                     ; 関数の参照先を参照
+(bind-key "C-M-/" 'helm-gtags-find-symbol)                   ; シンボルの定義先を参照
 
 ;; M-
 (bind-key* "M-k" 'kill-buffer-for-elscreen)                  ; カレントバッファを閉じる
