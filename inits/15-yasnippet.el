28abf04acbbe496f2a1ba44c89756b6024bec90a
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-08-16 00:24:20 +0900

    yasnippet更新

--- inits/15-yasnippet.el
+++ inits/15-yasnippet.el
@@ -8,10 +8,10 @@
 ;; ;; ~/.emacs.d/にsnippetsというフォルダを作っておきましょう
 ;; (setq yas-snippet-dirs
 ;;       '("~/.emacs.d/share/snippets"))
-;; (yas-global-mode 1)
+(yas-global-mode 1)
 ;; (yas-load-directory "~/.emacs.d/share/snippets")
 
 ;; ;; 単語展開キーバインド (ver8.0から明記しないと機能しない)
 ;; ;; (setqだとtermなどで干渉問題ありでした)
 ;; ;; もちろんTAB以外でもOK 例えば "C-;"とか
-;; (custom-set-variables '(yas-trigger-key "TAB"))
+(custom-set-variables '(yas-trigger-key "TAB"))
