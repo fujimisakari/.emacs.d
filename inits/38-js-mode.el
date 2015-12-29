aa7aae836c20d5383a41d475daecb12d958f3a1b
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-12-29 12:02:20 +0900

    js-modeをangular-modeに変更

--- inits/38-js-mode.el
+++ inits/38-js-mode.el
@@ -6,8 +6,10 @@
 
 ;; js2-mode
 (autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
+
+(add-to-list 'auto-mode-alist '("\\.js$" . angular-mode))
 
 ;; (defun js-indent-hook ()
 ;;   ;; インデント幅を2にする
