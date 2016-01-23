58f30c9014feb8806fec760872845ce7b4b6b71b
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2016-01-24 01:21:36 +0900

    markdown-modeにプレビュー機能を追加

--- inits/42-markdown-mode.el
+++ inits/42-markdown-mode.el
@@ -12,4 +12,17 @@
   (occur "^\\(#+\\|.*\n===+\\|.*\n\---+\\)")
   (other-window 1))
 
+;; プレビュー
+(defun markdown-preview-by-eww ()
+  (interactive)
+  (message (buffer-file-name))
+  (call-process "grip" nil nil nil
+                (buffer-file-name)
+                "--export"
+                "/tmp/grip.html")
+  (let ((buf (current-buffer)))
+    (eww-open-file "/tmp/grip.html")
+    (switch-to-buffer buf)
+    (pop-to-buffer "*eww*")))
+
 ;;; 42-markdown-mode.el ends here
