3467661512c81b28948c652307fbb9b9cdca7788
Fujimisakari <fujimisakari@gmail.com>
2017-01-13 17:18:48 +0900

    Modify web-mode

--- inits/36-web-mode.el
+++ inits/36-web-mode.el
@@ -8,7 +8,7 @@
 
 ;; 適用する拡張子
 (add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
