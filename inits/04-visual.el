d90ecaa05bf18411ba08d1bc7edfbccfae02765e
Fujimisakari <fujimisakari@gmail.com>
2017-01-13 17:17:03 +0900

    Modify rainbow-delimiters color

--- inits/04-visual.el
+++ inits/04-visual.el
@@ -114,13 +114,12 @@
 (set-face-foreground 'rainbow-delimiters-depth-1-face "SlateBlue2")
 (set-face-foreground 'rainbow-delimiters-depth-2-face "DarkOliveGreen2")
 (set-face-foreground 'rainbow-delimiters-depth-3-face "CornflowerBlue")
-(set-face-foreground 'rainbow-delimiters-depth-4-face "khaki2")
 (set-face-foreground 'rainbow-delimiters-depth-4-face "PaleGreen2")
-(set-face-foreground 'rainbow-delimiters-depth-5-face "DarkSlateGray2")
-(set-face-foreground 'rainbow-delimiters-depth-6-face "LightSalmon2")
-(set-face-foreground 'rainbow-delimiters-depth-7-face "magenta2")
-(set-face-foreground 'rainbow-delimiters-depth-8-face "IndianRed4")
-(set-face-foreground 'rainbow-delimiters-depth-9-face "DeepPink3")
+(set-face-foreground 'rainbow-delimiters-depth-5-face "magenta2")
+(set-face-foreground 'rainbow-delimiters-depth-6-face "DarkSlateGray2")
+(set-face-foreground 'rainbow-delimiters-depth-7-face "khaki2")
+(set-face-foreground 'rainbow-delimiters-depth-8-face "DeepPink3")
+(set-face-foreground 'rainbow-delimiters-depth-9-face "LightSalmon2")
 
 ;; カーソル位置のフェースを調べる関数
 (defun describe-face-at-point ()
