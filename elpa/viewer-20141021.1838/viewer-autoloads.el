a197d8b3ef0f0620855ca0e7b424c6d76f4755c7
Fujimisakari <fujimisakari@gmail.com>
2017-01-22 23:15:13 +0900

    Update elpa

--- elpa/viewer-20141021.1838/viewer-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; viewer-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("viewer.el") (21781 31970 926051 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; viewer-autoloads.el ends here
--- elpa/viewer-20141021.1838/viewer-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "viewer" "20141021.1838" "View-mode extension" 'nil :url "http://www.emacswiki.org/cgi-bin/wiki/download/viewer.el" :keywords '("view" "extensions"))
