b738c706e5538050c3d451ea72b819ccda26f705
Fujimisakari <fujimisakari@gmail.com>
2018-07-01 14:08:04 +0900

    Add typescript-mode

---
+++ inits/53-typescript-mode.el
@@ -0,0 +1,10 @@
+;;; 53-typescript-mode.el --- typescript-mode設定 -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'typescript-mode)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+
+;;; 53-typescript-mode.el ends here
