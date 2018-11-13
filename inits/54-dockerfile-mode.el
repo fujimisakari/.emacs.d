9f083dd929dcdc09b49fef75fe75011cf3ee943d
Fujimisakari <fujimisakari@gmail.com>
2018-11-14 00:55:44 +0900

    add dockerfile-mode

---
+++ inits/54-dockerfile-mode.el
@@ -0,0 +1,9 @@
+;;; 54-dockerfile-mode.el --- dockerfile-mode設定 -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(add-to-list 'auto-mode-alist '("Dockerfile.*" . dockerfile-mode))
+
+;;; 54-dockerfile-mode.el ends here
