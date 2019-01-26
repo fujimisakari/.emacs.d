86642cb532bd85e3accec3f1d0a82f523af03a3f
Fujimisakari <fujimisakari@gmail.com>
2019-01-26 17:15:44 +0900

    add shellscript-mode

---
+++ inits/55-shellscript-mode.el
@@ -0,0 +1,10 @@
+;;; 55-shellscript-mode.el --- shellscript-mode設定 -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(setq sh-basic-offset 2)
+(setq sh-indentation 2)
+
+;;; 55-shellscript-mode.el ends here
