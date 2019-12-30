875edf65bdaaf32089eae6add3c3505083ea4f21
Fujimisakari <fujimisakari@gmail.com>
2019-12-31 01:27:59 +0900

    add golden-ratio-mode

--- inits/09-window.el
+++ inits/09-window.el
@@ -10,4 +10,12 @@
   (when (one-window-p) (split-window-horizontally))
   (other-window 1))
 
+;; 画面分割時にいい感じの比率にしてくれる
+(golden-ratio-mode 1)
+(setq golden-ratio-adjust-factor 3.
+      golden-ratio-wide-adjust-factor 3.)
+(setq golden-ratio-exclude-modes '("calendar-mode" "wl-folder-mode" "wl-summary-mode"))
+(setq golden-ratio-exclude-buffer-names '(" *Org tags*" " *Org todo*" "Foldor" "Summmary"))
+(setq golden-ratio-exclude-buffer-regexp '("\\*anything" "\\*helm"))
+
 ;;; 09-window.el ends here
