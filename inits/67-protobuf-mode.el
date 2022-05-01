e68f50a6b944a11d1bafd0821e7d1408f06da12a
Fujimisakari <fujimisakari@gmail.com>
2022-05-01 21:37:13 +0900

    update protobuf-mod

--- inits/67-protobuf-mode.el
+++ inits/67-protobuf-mode.el
@@ -6,36 +6,4 @@
 
 (require 'protobuf-mode)
 
-(defconst my-protobuf-style
-  '((c-basic-offset . 2)
-    (indent-tabs-mode . nil)))
-
-(add-hook 'protobuf-mode-hook
-          (lambda () (c-add-style "my-style" my-protobuf-style t)))
-
-(defun protobuf-mode ()
-  "Major mode for editing Protocol Buffers description language.
-
-The hook `c-mode-common-hook' is run with no argument at mode
-initialization, then `protobuf-mode-hook'.
-
-Key bindings:
-\\{protobuf-mode-map}"
-  (interactive)
-  (kill-all-local-variables)
-  (set-syntax-table protobuf-mode-syntax-table)
-  (setq major-mode 'protobuf-mode
-        mode-name "Protocol-Buffers"
-        local-abbrev-table protobuf-mode-abbrev-table
-        abbrev-mode t)
-  (use-local-map protobuf-mode-map)
-  (c-initialize-cc-mode t)
-  (if (fboundp 'c-make-emacs-variables-local)
-      (c-make-emacs-variables-local))
-  (c-init-language-vars protobuf-mode)
-  (c-common-init 'protobuf-mode)
-  (easy-menu-add protobuf-menu)
-  (c-run-mode-hooks 'c-mode-common-hook 'protobuf-mode-hook)
-  (c-update-modeline))
-
 ;;; 67-protobuf-mode.el ends here
