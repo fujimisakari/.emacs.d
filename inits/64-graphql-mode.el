78a12e40fe8b326cd04fbfbd7fd9ebf4b9e0c816
Fujimisakari <fujimisakari@gmail.com>
2023-10-28 23:10:41 +0900

    add graphql-mode

---
+++ inits/64-graphql-mode.el
@@ -0,0 +1,11 @@
+;;; 64-graphql.el --- graphql設定 -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'graphql)
+
+(add-to-list 'auto-mode-alist '("\\.graphqls$"   . graphql-mode))
+
+;;; 64-graphql.el ends here
