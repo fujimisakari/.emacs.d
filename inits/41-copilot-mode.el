1d8cbdd05e9bcea94863a1435451b14c16fa489d
Fujimisakari <fujimisakari@gmail.com>
2023-10-28 23:09:43 +0900

    add copilot-mode

---
+++ inits/41-copilot-mode.el
@@ -0,0 +1,11 @@
+;;; 41-copilot-mode --- copilot-mode設定 -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'copilot)
+
+(add-hook 'prog-mode-hook 'copilot-mode)
+
+;;; 41-copilot-mode ends here
