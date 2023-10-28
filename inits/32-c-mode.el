96c24cc53d9b84c6841a3caa89b7be08bec17b42
Fujimisakari <fujimisakari@gmail.com>
2023-10-28 22:58:49 +0900

    update c-mode

--- inits/32-c-mode.el
+++ inits/32-c-mode.el
@@ -9,10 +9,27 @@
             (c-set-style "stroustrup")
             (common-mode-init)))
 
-(custom-set-variables
- '(ac-clang-cflags '("-I/usr/include")))
+;; .hと.mを左右に並べて開く
+(defun open-header-and-source-file ()
+  (interactive)
+  (other-window-or-split)
+  (ff-find-other-file))
 
-(require 'ccls)
-(setq ccls-executable "/usr/local/bin/ccls")
+;;コード整形できるようにする
+(require 'clang-format)
+
+(defun clang-format-save-hook-for-this-buffer ()
+  "Create a buffer local save hook."
+  (add-hook 'before-save-hook
+            (lambda ()
+              (when (locate-dominating-file "." ".clang-format")
+                (clang-format-buffer))
+              ;; Continue to save.
+              nil)
+            nil
+            ;; Buffer local hook.
+            t))
+;; Run this for each mode you want to use the hook.
+(add-hook 'c-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
 
 ;;; 32-c-mode.el ends here
