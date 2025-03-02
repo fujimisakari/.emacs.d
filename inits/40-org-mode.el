b02256b60d69d5c1a96616a125a2ea800092f21d
Fujimisakari <fujimisakari@gmail.com>
2025-03-02 13:27:31 +0900

    update org-mode setting

--- inits/40-org-mode.el
+++ inits/40-org-mode.el
@@ -3,7 +3,7 @@
 ;;; Commentary:
 
 ;;; Code:
-(require 'ox-reveal)
+
 (require 'org)
 
 ;; 基本設定
@@ -13,7 +13,7 @@
 (setq org-startup-with-inline-images t)        ; 画像をインライン表示
 (setq org-indent-mode-turns-on-hiding-stars t) ; 見出しインデントのアスタリスクを減らす
 (setq org-return-follows-link t)               ; リンクはRETで開く
-(setq org-edit-src-content-indentation 0)      ; BEGIN_SRCブロック内をインデントをしない
+(setq org-image-actual-width 1100)             ; 画像のデフォルト幅を指定
 
 ; 拡張子がorgのファイルを開いた場合、自動的にorg-modeにする
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
@@ -50,24 +50,48 @@
 ;; src のハイライト設定
 (setq org-src-fontify-natively t)
 
-(defface org-block-begin-line
-  '((t (:foreground "DimGray" :background "DarkSlateGray")))
-  "Face used for the line delimiting the begin of source blocks.")
-(set-face-foreground 'org-block-begin-line "DimGray")
-;; (set-face-background 'org-block-begin-line "gray18")
+;; (defface org-block-begin-line
+;;   '((t (:foreground "DimGray" :background "DarkSlateGray")))
+;;   "Face used for the line delimiting the begin of source blocks.")
+;; (set-face-foreground 'org-block-begin-line "DimGray")
+;; ;; (set-face-background 'org-block-begin-line "gray18")
 
-(defface org-block-background
-  '((t (:background "gray18")))
-  "Face used for the source block background.")
-(set-face-background 'org-block-background "gray18")
+;; (defface org-block-background
+;;   '((t (:background "gray18")))
+;;   "Face used for the source block background.")
+;; (set-face-background 'org-block-background "gray18")
 
-(defface org-block-end-line
-  '((t (:foreground "DimGray" :background "gray18")))
-  "Face used for the line delimiting the end of source blocks.")
-(set-face-foreground 'org-block-end-line "DimGray")
-;; (set-face-background 'org-block-end-line "gray18")
+;; (defface org-block-end-line
+;;   '((t (:foreground "DimGray" :background "gray18")))
+;;   "Face used for the line delimiting the end of source blocks.")
+;; (set-face-foreground 'org-block-end-line "DimGray")
+;; ;; (set-face-background 'org-block-end-line "gray18")
 
 (set-face-foreground 'org-level-5 "orange")  ; レベル3の色とカブってたので変更
 (set-face-foreground 'org-level-7 "purple1") ; レベル5の色とカブってたので変更
+
+;; 画像貼り付け
+;; https://chatgpt.com/share/0ca4b7b0-ecc6-41c3-9454-9588aefba8e4
+(defun my/copy-latest-file-and-insert-org-link (source-dir target-dir)
+  "Copy the latest file from SOURCE-DIR to a subdirectory of TARGET-DIR based on the current date,
+d insert the org-mode image link at point."
+  (interactive "DSource directory: \nDTarget directory: ")
+  (let* ((files (directory-files source-dir t "^[^.].*png"))
+         (latest-file (car (sort files (lambda (a b) (time-less-p (nth 5 (file-attributes b))
+                                                                  (nth 5 (file-attributes a)))))))
+         (file-name (file-name-nondirectory latest-file))
+         (current-year (format-time-string "%Y"))
+         (current-month (format-time-string "%m"))
+         (target-subdir (expand-file-name (concat current-year "/" current-month) target-dir))
+         (target-path (expand-file-name file-name target-subdir)))
+    (unless (file-directory-p target-subdir)
+      (make-directory target-subdir t))
+    (copy-file latest-file target-path t)
+    (insert (format "[[file:%s]]" target-path))
+    (message "Copied file to: %s" target-path)))
+
+(defun my/insert-image-like-logsec ()
+  (interactive)
+  (my/copy-latest-file-and-insert-org-link "~/Desktop" "~/org/work/img"))
 
 ;;; 40-org-mode.el ends here
