56e9f1539d9178642dfc942b9636cc29a0dc4ef8
Fujimisakari <fujimisakari@gmail.com>
2025-03-02 10:51:47 +0900

    update copilot-mode to change max limit setting

--- inits/41-copilot-mode.el
+++ inits/41-copilot-mode.el
@@ -6,6 +6,9 @@
 
 (require 'copilot)
 
+;; 大きなファイルでもCopilotを使用したい場合、copilot-max-charの制限を引き上げることができます。例えば、5,000,000文字に設定してみると良いでしょう
+(setq copilot-max-char 5000000)
+
 ;; (add-hook 'prog-mode-hook 'copilot-mode)
 
 ;;; 41-copilot-mode ends here
