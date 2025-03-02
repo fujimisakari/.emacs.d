48e0c87c9ab00bd0dd1475681cbede5f75b61a30
Fujimisakari <fujimisakari@gmail.com>
2025-03-02 13:34:19 +0900

    delete (package-initialize)

--- init.el
+++ init.el
@@ -10,7 +10,6 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
 
 (defun add-to-load-path (&rest paths)
   (let (path)
