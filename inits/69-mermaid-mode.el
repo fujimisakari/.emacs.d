ed3d11303ac5ce584fee351eb711146f510415ce
Fujimisakari <fujimisakari@gmail.com>
2025-09-10 21:16:05 +0900

    add mermaid-mode.el

---
+++ inits/69-mermaid-mode.el
@@ -0,0 +1,11 @@
+;;; 69-mermaid-mode.el --- mermaid-mode設定 -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'mermaid-mode)
+(setq mermaid-output-format ".svg")
+(setq mermaid-tmp-dir "/tmp/mermaid")
+
+;;; 69-mermaid-mode.el ends here
