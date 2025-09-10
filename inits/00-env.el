d5f188de391b9b2218a91cf49089a2fa99281962
Fujimisakari <fujimisakari@gmail.com>
2025-09-10 21:08:08 +0900

    update env.el

--- inits/00-env.el
+++ inits/00-env.el
@@ -29,6 +29,8 @@
 (load-file (expand-file-name "~/.emacs.d/share/shellenv/work1_shellenv.el"))
 (load-file (expand-file-name "~/.emacs.d/share/shellenv/work2_shellenv.el"))
 (load-file (expand-file-name "~/.emacs.d/share/shellenv/work3_shellenv.el"))
+(load-file (expand-file-name "~/.emacs.d/share/shellenv/work4_shellenv.el"))
+(load-file (expand-file-name "~/.emacs.d/share/shellenv/work5_shellenv.el"))
 (load-file (expand-file-name (format "~/.emacs.d/share/shellenv/%s_shellenv.el" (getenv "USER"))))
 (dolist (path (reverse (split-string (getenv "PATH") ":")))
   (add-to-list 'exec-path path))
