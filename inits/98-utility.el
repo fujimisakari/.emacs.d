f40eafa8860f3a6b7d2275226b54739106f8eefe
Fujimisakari <fujimisakari@gmail.com>
2025-09-10 21:14:31 +0900

    delete w3m.el

--- inits/98-utility.el
+++ inits/98-utility.el
@@ -7,11 +7,7 @@
 (defun open-browse-by-url ()
   "w3m上のリンクやURL文字列をブラウザで開く"
   (interactive)
-  (setq w3m-url (w3m-print-this-url))
-  (if (> (length w3m-url) 0)
-      (browse-url w3m-url browse-url-new-window-flag)
-    (browse-url-at-point)))
-
+  (browse-url-at-point))
 
 ;; windowを入れ変える
 ;; http://emacswiki.org/emacs/TransposeWindowsより参照
