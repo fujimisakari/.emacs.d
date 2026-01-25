089868a0615eaaac4b9c018ed9bdc64dc4b1a562
Fujimisakari <fujimisakari@gmail.com>
2026-01-25 22:54:32 +0900

    update pdf-viewer.el

--- inits/65-pdf-viewer.el
+++ inits/65-pdf-viewer.el
@@ -10,4 +10,9 @@
     (setenv "PKG_CONFIG_PATH" "/usr/local/opt/libffi/lib/pkgconfig"))
 (pdf-tools-install t)
 
+(defun my/disable-line-numbers-for-pdf-view ()
+  (display-line-numbers-mode -1))
+
+(add-hook 'pdf-view-mode-hook #'my/disable-line-numbers-for-pdf-view)
+
 ;;; 65-pdf-viewer.el ends here
